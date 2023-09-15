7f7d22cfda8bb026d3f6bbf09d4119bedb9c07bc
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2023-09-15 12:15:48 +0200

    modifica

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -9,7 +9,7 @@ function(VulkanCm_setup_dependencies)
   # already been provided to us by a parent project
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+    cpmaddpackage("gh:fmtlib/fmt#10.1.1")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
@@ -17,23 +17,15 @@ function(VulkanCm_setup_dependencies)
       NAME
       spdlog
       VERSION
-      1.11.0
+      1.12.0
       GITHUB_REPOSITORY
       "gabime/spdlog"
       OPTIONS
       "SPDLOG_FMT_EXTERNAL ON")
   endif()
 
-  if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
-  endif()
-
   if(NOT TARGET CLI11::CLI11)
     cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
-  endif()
-
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI#e23dbc7473654024852ede60e2121276c5aab660")
   endif()
 
   if(NOT TARGET tools::tools)
